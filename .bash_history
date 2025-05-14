c75d58c43291cc29ca2f291e5eb5caf350ab96cc
mlopezbadias <152566466+mlopezbadias@users.noreply.github.com>
2025-05-14 16:41:25 -0300

    seteo pero bien

---
+++ .bash_history
@@ -0,0 +1,14 @@
+cd /root
+cd Toolchain
+ls
+cd ..
+ls
+cd /root
+clone exit
+exit
+cd /root
+cd Toolchain
+make all
+cd ..
+make all
+exit
---
+++ compile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CONTAINER_NAME=TPE-ARQUI-1C2025
+
+docker start $CONTAINER_NAME
+
+docker exec -it $CONTAINER_NAME make clean -C /root/Toolchain
+docker exec -it $CONTAINER_NAME make clean -C /root
+
+docker exec -it $CONTAINER_NAME make -C /root/Toolchain
+
+docker exec -it $CONTAINER_NAME make -C /root
+
+docker stop $CONTAINER_NAME

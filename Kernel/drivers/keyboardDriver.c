8297a44c3d3e614e7a58d12d3e7118ef0837ca14
mlopezbadias <152566466+mlopezbadias@users.noreply.github.com>
2025-05-15 22:58:06 -0300

    define utility keys

--- Kernel/drivers/keyboardDriver.c
+++ Kernel/drivers/keyboardDriver.c
@@ -3,6 +3,17 @@
 
 #define KEYS 58
 
+// utility keys
+#define ESC 0x01
+#define BACK 0x0E
+#define TAB 0x0F
+#define ENTER 0x1C
+#define CAPS 0x3A
+#define LSHIFT 0x2A
+#define RSHIFT 0x36
+#define CTRL 0x1D
+#define ALT 0x38
+
 
 static unsigned char keyboard[KEYS][2] = {
 	{0, 0}, {27, 27}, {'1', '!'}, {'2', '@'}, {'3', '#'}, {'4', '$'}, {'5', '%'}, {'6', '^'}, {'7', '&'}, {'8', '*'}, {'9', '('}, {'0', ')'}, {'-', '_'}, {'=', '+'}, {'\b', '\b'},
